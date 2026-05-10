02a2b2f34099b8ebe57b21791eac3bc576dfb146
Dave <migichidave09@gmail.com>
2026-05-10 13:51:35 +0300

    Updated project features and fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const SYSTEM_LOGO_URL = 'https://postimg.cc/H8wJRGP6';
+export const SYSTEM_LOGO_URL = 'https://i.postimg.cc/GtNkBdPn/DMi-logo.jpg';
 export const BRAND_LOGO_URL = 'https://storage.googleapis.com/static.antigravity.dev/projects/kayb6z7vprmlkln2iwpxb5/attachments/42f9b841-8608-410a-85d0-99c58550787e.png';
-export const DMI_FALLBACK_ICON = 'https://postimg.cc/H8wJRGP6';
+export const DMI_FALLBACK_ICON = 'https://i.postimg.cc/GtNkBdPn/DMi-logo.jpg';
