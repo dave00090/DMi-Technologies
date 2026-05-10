f15aa1014247084ce288604c7060c90263079c56
Dave <migichidave09@gmail.com>
2026-05-10 13:33:27 +0300

    Updated project features and fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const SYSTEM_LOGO_URL = 'https://postimg.cc/H8wJRGP6';
 export const BRAND_LOGO_URL = 'https://storage.googleapis.com/static.antigravity.dev/projects/kayb6z7vprmlkln2iwpxb5/attachments/42f9b841-8608-410a-85d0-99c58550787e.png';
-export const DMI_FALLBACK_ICON = 'https://cdn-icons-png.flaticon.com/512/1055/1055644.png';
+export const DMI_FALLBACK_ICON = 'https://postimg.cc/H8wJRGP6';
